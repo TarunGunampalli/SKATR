2a168a73081001f56e25ed474938452302e52199
anujjain314 <anujjain314@utexas.edu>
2020-10-25 08:58:07 -0500

    sheet population and name, email bot commands added

--- bot.py
+++ bot.py
@@ -1,18 +1,64 @@
-import discord
-intents = discord.Intents.default()
-intents.members = True
-
+from sheetsEX import users_sheet
 from dotenv import load_dotenv
 import os
-load_dotenv('.env')
+import discord
+from discord.ext import commands
 
-client = discord.Client(intents=intents)
+# Discord Bot Token
+load_dotenv('.env')
 TOKEN = os.getenv('TOKEN')
 
-def get_member_names():
+# Initializes Discord privileged gateway intents
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+names = []
+ids = []
+pfp_urls = []
+
+bot = commands.Bot(command_prefix="!", case_insensitive=True)
+
+# Gets all members names from servers
+def get_members():
     for guild in client.guilds:
         for member in guild.members:
-            yield member.name
+            names.append(member.name)
+            ids.append(member.id)
+            pfp_urls.append(str(member.avatar_url))
+
+
+# def get_member_ids():
+#     for guild in client.guilds:
+#         for member in guild.members:
+#             yield member.id
+
+# Bot login initialization
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name="name")
+async def test(ctx, *, arg):
+    await ctx.send(arg)
+    id = ctx.message.author.id
+    cell = users_sheet.find(str(id))
+    users_sheet.update_cell(cell.row, cell.col + 2, arg)
+
+
+@bot.command(name="email")
+async def test(ctx, *, arg):
+    await ctx.send(arg)
+    id = ctx.message.author.id
+    cell = users_sheet.find(str(id))
+    users_sheet.update_cell(cell.row, cell.col + 3, arg)
+
+@bot.command(name="password")
+async def test(ctx, *, arg):
+    await ctx.send(arg)
+    id = ctx.message.author.id
+    cell = users_sheet.find(str(id))
+    users_sheet.update_cell(cell.row, cell.col + 4, arg)
 
 @client.event
 async def on_ready():
@@ -23,13 +69,37 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# Populates main sheet with member names and ids
+    if message.content.startswith('!populate_users'):
+        get_members()
+        counter = 0
+        for id in ids:
+            counter += 1
+            users_sheet.update_cell(counter + 1, 1, str(id))
+        counter = 0
+        for name in names:
+            counter += 1
+            users_sheet.update_cell(counter + 1, 2, name)
+        print(users_sheet.cell(2, 1).value)
+        counter = 0
+        print(pfp_urls)
+        for pfp in pfp_urls:
+            counter += 1
+            users_sheet.update_cell(counter + 1, 3, pfp)
 
-    if message.content.startswith('!members'):
-        people = set(get_member_names())
-        for person in people:
-            await message.channel.send(person)
+        await message.channel.send('Populated sheet!')
 
+# # Populates relationship sheet with member
+#     if message.content.startswith('!populate_questions'):
+#         get_members()
+#         counter = 0
+#         for id in ids:
+#             counter += 1
+#             match_survey.update_cell(1, counter + 1, str(id))
+#             macth_survey.update_cell(counter + 1, 1, str(id))
+#             q2_sheet.update_cell(1, counter + 1, str(id))
+#             q2_sheet.update_cell(counter + 1, 1, str(id))
+#             q3_sheet.update_cell(1, counter + 1, str(id))
+#             q3_sheet.update_cell(counter + 1, 1, str(id))
 
 client.run(TOKEN)
--- sheetsEX.py
+++ sheetsEX.py
@@ -1,6 +1,3 @@
-#pip install gspread
-#pip install --upgrade google-api-python-client oauth2client
-
 import gspread
 #import pandas as pd 
 from oauth2client.service_account import ServiceAccountCredentials 
@@ -10,17 +7,4 @@ scope = ['https://spreadsheets.google.com/feeds',
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope) #has info for service account
 client = gspread.authorize(creds)
 
-sheet = client.open("commentary data").sheet1   #commentary data is a test sheet full of cricket plays
-
-#list_of_hashes = sheet.get_all_records()
-ids_of_players = sheet.col_values(8)
-print(ids_of_players)
-
-#sheet.get_all_values() list of lists instead of hashes
-#Or you could just pull the data from a single row, column, or cell:
-
-#sheet.row_values(1) select a row
-
-#sheet.col_values(1) select a column probably most useful
-
-#sheet.cell(1, 1).value select single cell
+users_sheet = client.open("SKATRdb").get_worksheet(0)
